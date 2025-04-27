b281b5861481e4087dcad7fbe22b93951f314055
SaQib-1237 <saqiblogic50@gmail.com>
2025-04-27 12:23:08 +0500

    fix Code

--- Registration/index.js
+++ Registration/index.js
@@ -1,57 +1,31 @@
 
-// function handleSubmit(event) {
+function handleSubmit(event) {
 
-//     // Erase The Form Default Behavior
-//     event.preventDefault();
-//     // Form Reference
-//     let form = event.target;
-//     let userName = form.username.value
-//     let userEmail = form.useremail.value
-//     let userPassword = form.password.value
+    // Erase The Form Default Behavior
+    event.preventDefault();
+    // Form Reference
+    let form = event.target;
+    let userName = form.username.value
+    let userEmail = form.useremail.value
+    let userPassword = form.password.value
     
-//     if (localStorage.getItem('userEmail') === userEmail) {
-//       checkUser();
-//       document.querySelector('#registerd-pera').innerText = 'If You have Registerd Then';
-//       document.querySelector('#registerd-btn').innerText = 'login';
-//       return;
-//     }
+    if (localStorage.getItem('userEmail') === userEmail) {
+      checkUser();
+      document.querySelector('#registerd-pera').innerText = 'If You have Registerd Then';
+      document.querySelector('#registerd-btn').innerText = 'login';
+      return;
+    }
   
-//   localStorage.setItem('userEmail', userEmail);
-//   localStorage.setItem('userPassword', userPassword);
-//   localStorage.setItem('userName', userName);
-
-//   document.querySelector('form').reset();
-
-//   alert('User Register Successfully');
-
-// }
-
-// function checkUser() {
-//   alert('User Already Exist');
-// }
-
-
-let users = JSON.parse(localStorage.getItem('users')) || [];
-
-function addUser(event) {
-  // remove Default Behavior
-  event.preventDefault();
-  // Geting Form Reference
-  let form = event.target;
-  let userName = form.username.value;
-  let userEmail = form.useremail.value;
-  let  userPassword = form.password.value;
-
-  if (users.some(checkUser => checkUser.email === userEmail)) {
-    alert('User Already Exist');
-    return;
-  }
-
-  users.push({name:userName, email:userEmail, password:userPassword});
-  localStorage.setItem('users', JSON.stringify(users));
+  localStorage.setItem('userEmail', userEmail);
+  localStorage.setItem('userPassword', userPassword);
+  localStorage.setItem('userName', userName);
 
   document.querySelector('form').reset();
 
-  alert('User Registerd SuccessFully');
+  alert('User Register Successfully');
 
+}
+
+function checkUser() {
+  alert('User Already Exist');
 }
